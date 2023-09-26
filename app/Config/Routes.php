c93271c468e329458703664acb90b65df342aa29
CheeneXiManalo <cheeneximanalo05@gmail.com>
2023-09-26 14:07:54 +0800

    main

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'MusicController::index');
+$routes->post('music/createPlaylist', 'MusicController::createPlaylist');
+$routes->post('music/upload', 'MusicController::uploadMusic');
+$routes->post('music/getPlaylistMusic', 'MusicController::getPlaylistMusic');
+$routes->post('music/addToPlaylist', 'MusicController::addToPlaylist'); 
